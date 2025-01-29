aefa5b28229e5d7a4f9be6a42232a04f03164a89
mark-gibson-44 <mjgibson44@outlook.com>
2025-01-29 21:14:08 +0000

    009 Enhanced existing benchmark framework

--- benchmarks/vector/vector_bench.cpp
+++ benchmarks/vector/vector_bench.cpp
@@ -1,8 +1,16 @@
+#include <iostream>
+#include <chrono>
 #include <cstdint>
 #include <vector>
 
+#include "vec.h"
+// Todo, think about how to mess with system calls for better affinity
 int main()
 {
+    
+    const std::chrono::time_point<std::chrono::high_resolution_clock> start =
+        std::chrono::high_resolution_clock::now();
+
     std::vector<uint64_t> test;
     constexpr uint64_t loopNum = 0x7FFFFFF;
     test.reserve(loopNum);
@@ -11,6 +19,30 @@ int main()
         test.push_back(i);
     }
     
+    const std::chrono::time_point<std::chrono::high_resolution_clock> Finish1 =
+        std::chrono::high_resolution_clock::now();
+
+    const std::chrono::time_point<std::chrono::high_resolution_clock> Start2 =
+        std::chrono::high_resolution_clock::now();
+
+    Benchstl::vector<uint64_t, std::allocator<uint64_t>> test2(0, std::allocator<uint64_t>());
+    test2.reserve(loopNum);
+    
+    for(uint64_t i = 1; i < loopNum; i++)
+    {
+        test2.push_back(i);
+    }
+    
+    const std::chrono::time_point<std::chrono::high_resolution_clock> Finish2 =
+        std::chrono::high_resolution_clock::now();
+    
+
+
+    double std_elapsed_time_ms = std::chrono::duration<double, std::milli>(Finish1-start).count();
+    double impl_elapsed_time_ms = std::chrono::duration<double, std::milli>(Finish2 - Start2).count();
+
+    std::cout << "STD " << std_elapsed_time_ms << '\n';
+    std::cout << "IMPL " << impl_elapsed_time_ms << '\n';
     return 0;
 
 }
